0cb65ff8d276e7f0176dcd3bdbd9e6b2506a8a4c
Addhyan01 <addhyankumar@gmail.com>
2024-09-21 05:05:35 +0530

    all Api is working all set

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.post("/register", async (req, resp) => {
 
 
 app.post("/login", async (req, resp) => {
-    console.log(req.body);
+    
     if (req.body.password && req.body.email) {
         let user = await User.findOne(req.body).select("-password");
         if (user) {
@@ -48,29 +48,63 @@ app.post("/login", async (req, resp) => {
 })
 
 
-app.post('/add-product', async (req, resp)=>{
-        let product = new Product(req.body);
-        let result = await product.save();
-        resp.send(result);
+app.post('/add-product', async (req, resp) => {
+    let product = new Product(req.body);
+    let result = await product.save();
+    resp.send(result);
 })
 
 
-app.get('/products', async (req, resp)=>{
+app.get('/products', async (req, resp) => {
     let products = await Product.find();
-    if(products.length > 0){
+    if (products.length > 0) {
         resp.send(products);
     }
-    else{
-        resp.send({result:"No Product Found"});
+    else {
+        resp.send({ result: "No Product Found" });
 
     }
 
 })
 
 
-app.delete('/product/:id', async (req, resp)=>{
+app.delete('/product/:id', async (req, resp) => {
     // resp.send(req.params.id);
-    const result =await Product.deleteOne({_id:req.params.id})
+    const result = await Product.deleteOne({ _id: req.params.id })
+    resp.send(result)
+})
+
+
+app.get("/product/:id", async (req, resp) => {
+    let result = await Product.findOne({ _id: req.params.id });
+    if (result) {
+        resp.send(result);
+    } else {
+        resp.send({ result: "No Record Found" })
+    }
+});
+
+app.put("/product/:id", async (req, resp) => {
+    let result = await Product.updateOne(
+        { _id: req.params.id },
+        {
+            $set: req.body
+        }
+    )
+    resp.send(result)
+
+});
+
+
+app.get("/search/:key", async (req, resp) => {
+    let result = await Product.find({
+        "$or": [
+            { name: { $regex: req.params.key } },
+            { company: { $regex: req.params.key } },
+            { category: { $regex: req.params.key } },
+            { price: { $regex: req.params.key } }
+        ]
+    });
     resp.send(result)
 })
 
